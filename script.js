6320d1a26a2269c874912279ae82560b087dc177
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2024-07-04 13:16:12 +0900

    Can put marks on the board

--- script.js
+++ script.js
@@ -1,122 +1,84 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var Stage;
-(function (Stage) {
-    Stage[Stage["opponentSelection"] = 0] = "opponentSelection";
-    Stage[Stage["setUp"] = 1] = "setUp";
-    Stage[Stage["play"] = 2] = "play";
-})(Stage || (Stage = {}));
 var Position;
 (function (Position) {
-    Position[Position["topleft"] = 0] = "topleft";
-    Position[Position["topcenter"] = 1] = "topcenter";
-    Position[Position["topright"] = 2] = "topright";
-    Position[Position["centerleft"] = 3] = "centerleft";
-    Position[Position["center"] = 4] = "center";
-    Position[Position["centerright"] = 5] = "centerright";
-    Position[Position["bottomleft"] = 6] = "bottomleft";
-    Position[Position["bottomcenter"] = 7] = "bottomcenter";
-    Position[Position["bottomright"] = 8] = "bottomright";
+    Position["topleft"] = "topleft";
+    Position["topcenter"] = "topcenter";
+    Position["topright"] = "topright";
+    Position["centerleft"] = "centerleft";
+    Position["center"] = "center";
+    Position["centerright"] = "centerright";
+    Position["bottomleft"] = "bottomleft";
+    Position["bottomcenter"] = "bottomcenter";
+    Position["bottomright"] = "bottomright";
 })(Position || (Position = {}));
 //* Game Logic
-var gameboard = (function () {
+function GameBoard() {
     var board = new Map();
-    board.set('topleft', '');
-    board.set('topcenter', '');
-    board.set('topright', '');
-    board.set('centerleft', '');
-    board.set('center', '');
-    board.set('centerright', '');
-    board.set('bottomleft', '');
-    board.set('bottomcenter', '');
-    board.set('bottomright', '');
+    board.set(Position.topleft, ' ');
+    board.set(Position.topcenter, ' ');
+    board.set(Position.topright, ' ');
+    board.set(Position.centerleft, ' ');
+    board.set(Position.center, ' ');
+    board.set(Position.centerright, ' ');
+    board.set(Position.bottomleft, ' ');
+    board.set(Position.bottomcenter, ' ');
+    board.set(Position.bottomright, ' ');
     var setMark = function (position, mark) {
         board.set(position, mark);
     };
     var isPositionMarked = function (position) {
-        return board.get(position) !== '';
+        return board.get(position) !== ' ';
     };
-    return { setMark: setMark, isPositionMarked: isPositionMarked };
-})();
-var game = (function (board) {
-    var playerOne = null;
-    var playerTwo = null;
-    var gameBoard = board;
-    var checkWinner = function (gameboard) { return "playerOne"; };
-    var decideWinner = function (winner) { return 'void'; };
-    var changeTurns = function (player1, player2) { return 'void'; };
-    var makePlay = function (position) { return 'void'; }; //* Can only be called if stage is 'play'
-    var startGame = function () { };
-    var isComputerTurn = function (player2) { return true; };
-    var reset = function () { };
-    return { playerOne: playerOne, playerTwo: playerTwo, gameBoard: gameBoard, checkWinner: checkWinner, decideWinner: decideWinner, changeTurns: changeTurns, makePlay: makePlay, startGame: startGame, isComputerTurn: isComputerTurn, reset: reset };
-})(gameboard);
-var gameConfig = (function () {
-    var stage = Stage.opponentSelection;
+    var getMark = function (position) {
+        return board.get(position);
+    };
+    var showBoard = function () {
+        console.log(board.get(Position.topleft), board.get(Position.topcenter), board.get(Position.topright));
+        console.log(board.get(Position.centerleft), board.get(Position.center), board.get(Position.centerright));
+        console.log(board.get(Position.bottomleft), board.get(Position.bottomcenter), board.get(Position.bottomright));
+    };
+    return { setMark: setMark, isPositionMarked: isPositionMarked, getMark: getMark, showBoard: showBoard };
+}
+function Player(name, playerType, mark) {
+    var getMark = function () { return mark; };
+    var playerTurn = false;
+    var togglePlayerTurn = function () { playerTurn ? playerTurn = false : playerTurn = true; };
+    return { name: name, getMark: getMark, togglePlayerTurn: togglePlayerTurn, playerTurn: playerTurn, playerType: playerType };
+}
+var game = (function () {
     var playerOne;
     var playerTwo;
-    var setStage = function (newStage) { stage = newStage; };
-    var getStage = function () { return stage; };
-    var setPlayerInfo = function (playerInfo) {
-        if (stage === Stage.opponentSelection) {
-            setGame(playerInfo, game);
+    var gameBoard;
+    var setGame = function (name1, name2, opponentType) {
+        playerOne = createPlayer(name1, "O", "human");
+        if (opponentType === "computer")
+            playerTwo = createPlayer("computer", "X", "computer");
+        else
+            playerTwo = createPlayer(name2, "X", "human");
+        gameBoard = GameBoard();
+        console.log("".concat(playerOne.name + " " + playerOne.playerType, " vs ").concat(playerTwo.name + " " + playerTwo.playerType));
+        gameBoard.showBoard();
+        decideFirstPlayer();
+        if (playerTwo.playerTurn && playerTwo.playerType === "computer") {
+            var position = makeComputerPlay();
+            makePlay(position);
         }
     };
-    var setGame = function (newPlayerInfo, game) {
-        var opponent = newPlayerInfo.opponent, playerOneName = newPlayerInfo.playerOneName, playerTwoName = newPlayerInfo.playerTwoName;
-        playerOne = createPlayer(playerOneName, "O", "human");
-        if (opponent === "computer")
-            playerTwo = createComputer();
-        else
-            playerTwo = createPlayer(playerTwoName, "X", "human");
+    var decideFirstPlayer = function () {
         var randomNum = Math.floor(Math.random() * 2);
-        if (randomNum === 0)
-            playerOne.playerTurn = true;
-        else
-            playerTwo.playerTurn = true;
-        game.playerOne = __assign({}, playerOne);
-        game.playerTwo = __assign({}, playerTwo);
+        randomNum === 1 ? playerOne.playerTurn = true : playerTwo.playerTurn = true;
     };
-    return { getStage: getStage, setPlayerInfo: setPlayerInfo };
-})();
-var createPlayer = function (playerName, playerMark, type) {
-    var score = 0;
-    var mark = playerMark;
-    var name = playerName;
-    var playerType = type;
-    var playerTurn = false;
-    var increaseScore = function () { ++score; };
-    var getScore = function () { return score; };
-    var getMark = function () { return mark; };
-    var togglePlayerTurn = function () {
-        if (playerTurn)
-            playerTurn = true;
-        else
-            playerTurn = false;
-    };
-    return { name: name, playerType: playerType, increaseScore: increaseScore, getScore: getScore, getMark: getMark, togglePlayerTurn: togglePlayerTurn, playerTurn: playerTurn };
-};
-var createComputer = function () {
-    var player = createPlayer("Computer", "X", "computer");
-    var choosePosition = function (gameBoard) {
-        var positions = ['topleft',
-            'topcenter',
-            'topright',
-            'centerleft',
-            'center',
-            'centerright',
-            'bottomleft',
-            'bottomcenter',
-            'bottomright'
+    var getCurrentPlayer = function () { return playerOne.playerTurn ? playerOne : playerTwo; };
+    var makeComputerPlay = function () {
+        var positions = [
+            Position.topleft,
+            Position.topcenter,
+            Position.topright,
+            Position.centerleft,
+            Position.center,
+            Position.centerright,
+            Position.bottomleft,
+            Position.bottomcenter,
+            Position.bottomright
         ];
         while (true) {
             var randomNum = positions[Math.floor(Math.random() * 10)];
@@ -125,6 +87,61 @@ var createComputer = function () {
             }
         }
     };
-    return __assign(__assign({}, player), { choosePosition: choosePosition });
+    var checkWinner = function (gameboard) {
+    };
+    var decideWinner = function (winner) { return 'void'; };
+    var changeTurns = function () {
+        if (playerOne.playerTurn) {
+            playerOne.playerTurn = false;
+            playerTwo.playerTurn = true;
+        }
+        else {
+            playerOne.playerTurn = true;
+            playerTwo.playerTurn = false;
+        }
+    };
+    var makePlay = function (position) {
+        if (!gameBoard.isPositionMarked(position)) {
+            var currentPlayer = getCurrentPlayer();
+            var mark = currentPlayer.getMark();
+            gameBoard.setMark(position, mark);
+            gameBoard.showBoard();
+            changeTurns();
+        }
+    };
+    var reset = function () { };
+    return { setGame: setGame, makePlay: makePlay };
+})();
+var createPlayer = function (playerName, playerMark, type) {
+    var mark = playerMark;
+    var name = playerName;
+    var playerType = type;
+    var playerTurn = false;
+    var getMark = function () { return mark; };
+    var togglePlayerTurn = function () {
+        if (playerTurn)
+            playerTurn = true;
+        else
+            playerTurn = false;
+    };
+    return { name: name, playerType: playerType, getMark: getMark, togglePlayerTurn: togglePlayerTurn, playerTurn: playerTurn };
 };
-gameConfig.setPlayerInfo({ playerOneName: "Jeu", playerTwoName: "Kel", opponent: "computer" });
+var choosePosition = function (gameBoard) {
+    var positions = [
+        Position.topleft,
+        Position.topcenter,
+        Position.topright,
+        Position.centerleft,
+        Position.center,
+        Position.centerright,
+        Position.bottomleft,
+        Position.bottomcenter,
+        Position.bottomright
+    ];
+    while (true) {
+        var randomNum = positions[Math.floor(Math.random() * 10)];
+        if (!gameBoard.isPositionMarked(positions[randomNum])) {
+            return positions[randomNum];
+        }
+    }
+};
--- script.ts
+++ script.ts
@@ -1,172 +1,126 @@
-type Mark = "X" | "O"
+type Mark = "X" | "O" | " "
 type PlayerType = "human" | "computer"
 type Winner = "playerOne" | "playerTwo" | null
 
-enum Stage {
-    "opponentSelection", "setUp", "play"
-}
-
 enum Position {
-    'topleft',
-    'topcenter',
-    'topright',
-    'centerleft',
-    'center',
-    'centerright',
-    'bottomleft',
-    'bottomcenter',
-    'bottomright',
-}
-
-interface ScoreBoard {
-    showScore: (player1: Player, player2: Player) => void
-    makeAnnoucement: (message: string) => void
+    topleft = 'topleft',
+    topcenter = 'topcenter',
+    topright = 'topright',
+    centerleft = 'centerleft',
+    center = 'center',
+    centerright = 'centerright',
+    bottomleft = 'bottomleft',
+    bottomcenter = 'bottomcenter',
+    bottomright = 'bottomright',
 }
 
 interface Player {
-    increaseScore: () => void,
-    getScore: () => number,
     name: string,
     getMark: () => Mark,
     playerTurn: boolean,
     togglePlayerTurn: () => void,
-    readonly playerType: PlayerType,
+    playerType: PlayerType,
 
-}
-
-interface Computer extends Player {
-    choosePosition: (gameBoard: GameBoard) => string
 }
 
 interface GameBoard {
-    // board: Map<string, string>,
     setMark: (position: Position, mark: Mark) => void,
     isPositionMarked: (position: Position) => boolean
+    getMark: (position: Position) => Mark
+    showBoard: () => void
 }
 
 interface Game {
-    playerOne: Player | null
-    playerTwo: Player | null
-    gameBoard: GameBoard
-    checkWinner: (gameboard: GameBoard) => Winner
-    decideWinner: (winner: Winner) => void,
-    changeTurns: (player1: Player, player2: Player) => void,
+    setGame: (playerName1: string, playerName2: string, opponentType: PlayerType) => void
+    // getCurrentPlayer: (player1: Player, player2: Player | Computer) => Player | Computer
+    // checkWinner: (gameboard: GameBoard) => Winner
+    // decideWinner: (winner: Winner) => void,
+    // changeTurns: (player1: Player, player2: Player) => void,
     makePlay: (position: Position) => void, //* Can only be called if stage is 'play'
-    startGame: () => void,
-    isComputerTurn: (player2: Player) => boolean,
-    reset: () => void
+    // startGame: () => void,
+    // isComputerTurn: (player2: Player) => boolean,
+    // reset: () => void
 }
 
-interface PlayerInfo {
-    opponent: PlayerType,
-    playerOneName: string,
-    playerTwoName: string,
-}
-
-interface GameConfig {
-    // setStage: (newStage: Stage) => void,
-    getStage: () => Stage,
-    // setGame: (newPlayerInfo: PlayerInfo, game:Game) => void, //* Creates players
-    setPlayerInfo: (playerInfo: PlayerInfo) => void //* Can only be called if stage is 'config'
-}
 
 //* Game Logic
 
-let gameboard = ((): GameBoard => {
+function GameBoard(): GameBoard {
     const board = new Map()
-    board.set('topleft', '')
-    board.set('topcenter', '')
-    board.set('topright', '')
-    board.set('centerleft', '')
-    board.set('center', '')
-    board.set('centerright', '')
-    board.set('bottomleft', '')
-    board.set('bottomcenter', '')
-    board.set('bottomright', '')
+    board.set(Position.topleft, ' ')
+    board.set(Position.topcenter, ' ')
+    board.set(Position.topright, ' ')
+    board.set(Position.centerleft, ' ')
+    board.set(Position.center, ' ')
+    board.set(Position.centerright, ' ')
+    board.set(Position.bottomleft, ' ')
+    board.set(Position.bottomcenter, ' ')
+    board.set(Position.bottomright, ' ')
 
     const setMark = (position: Position, mark: Mark) => {
         board.set(position, mark)
     }
     const isPositionMarked = (position: Position) => {
-        return board.get(position) !== ''
+        return board.get(position) !== ' '
     }
 
-    return { setMark, isPositionMarked }
-})()
-
-let game = ((board: GameBoard): Game => {
-    let playerOne: Player | null = null
-    let playerTwo: Player | null | Computer = null
-    let gameBoard = board
-    const checkWinner = (gameboard: GameBoard) => ("playerOne" as Winner)
-    const decideWinner = (winner: Winner) => 'void'
-    const changeTurns = (player1: Player, player2: Player) => 'void'
-    const makePlay = (position: Position) => 'void' //* Can only be called if stage is 'play'
-    const startGame = () => { }
-    const isComputerTurn = (player2: Player) => true
-    const reset = () => {}
-
-   
-    return { playerOne, playerTwo, gameBoard, checkWinner, decideWinner, changeTurns, makePlay, startGame, isComputerTurn, reset }
-})(gameboard)
-
-const gameConfig = ((): GameConfig => {
-    let stage: Stage = Stage.opponentSelection
-    let playerOne: Player
-    let playerTwo: Player | Computer
-    const setStage = (newStage: Stage) => { stage = newStage }
-    const getStage = () => stage
-    const setPlayerInfo = (playerInfo: PlayerInfo) => {
-        if (stage === Stage.opponentSelection) {
-            setGame(playerInfo, game)
-        }
+    const getMark = (position: Position) => {
+        return board.get(position)
     }
-    const setGame = (newPlayerInfo: PlayerInfo, game:Game) => {
-        const { opponent, playerOneName, playerTwoName } = newPlayerInfo
-        playerOne = createPlayer(playerOneName, "O", "human")
-        if (opponent === "computer") playerTwo = createComputer()
-        else playerTwo = createPlayer(playerTwoName, "X", "human")
-        const randomNum = Math.floor(Math.random() * 2)
-        if (randomNum === 0) playerOne.playerTurn = true
-        else playerTwo.playerTurn = true
 
-        game.playerOne = {...playerOne}
-        game.playerTwo = {...playerTwo}
-
+    const showBoard = () => {
+        console.log(board.get(Position.topleft), board.get(Position.topcenter), board.get(Position.topright))
+        console.log(board.get(Position.centerleft), board.get(Position.center), board.get(Position.centerright))
+        console.log(board.get(Position.bottomleft), board.get(Position.bottomcenter), board.get(Position.bottomright))
     }
-    return { getStage, setPlayerInfo }
-})()
 
-const createPlayer = (playerName: string, playerMark: Mark, type: PlayerType): Player => {
-    let score: number = 0
-    const mark: Mark = playerMark
-    const name: string = playerName
-    const playerType: PlayerType | null = type
-    let playerTurn: boolean = false
-
-    const increaseScore = () => { ++score }
-    const getScore = () => score
-    const getMark = () => mark
-    const togglePlayerTurn = () => {
-        if (playerTurn) playerTurn = true
-        else playerTurn = false
-    }
-    return { name, playerType, increaseScore, getScore, getMark, togglePlayerTurn, playerTurn }
+    return { setMark, isPositionMarked, getMark, showBoard }
 }
 
-const createComputer = (): Computer => {
-    const player = createPlayer("Computer", "X", "computer")
+function Player(name: string, playerType: PlayerType, mark: Mark): Player {
+    const getMark = () => mark
+    let playerTurn = false
+    const togglePlayerTurn = () => { playerTurn ? playerTurn = false : playerTurn = true }
+    return { name, getMark, togglePlayerTurn, playerTurn, playerType }
+}
 
-    const choosePosition = (gameBoard: GameBoard) => {
-        const positions = ['topleft',
-            'topcenter',
-            'topright',
-            'centerleft',
-            'center',
-            'centerright',
-            'bottomleft',
-            'bottomcenter',
-            'bottomright'
+let game = ((): Game => {
+    let playerOne: Player
+    let playerTwo: Player
+    let gameBoard: GameBoard
+
+    const setGame = (name1: string, name2: string, opponentType: PlayerType,) => {
+        playerOne = createPlayer(name1, "O", "human")
+        if (opponentType === "computer") playerTwo = createPlayer("computer", "X", "computer")
+        else playerTwo = createPlayer(name2, "X", "human")
+        gameBoard = GameBoard()
+        console.log(`${playerOne.name + " " + playerOne.playerType} vs ${playerTwo.name + " " + playerTwo.playerType}`)
+        gameBoard.showBoard()
+        decideFirstPlayer()
+        if (playerTwo.playerTurn && playerTwo.playerType === "computer") {
+            let position = makeComputerPlay()
+            makePlay(position)
+        }
+    }
+
+    const decideFirstPlayer = () => {
+        const randomNum = Math.floor(Math.random() * 2)
+        randomNum === 1 ? playerOne.playerTurn = true : playerTwo.playerTurn = true
+    }
+
+    const getCurrentPlayer = () => playerOne.playerTurn ? playerOne : playerTwo
+
+    const makeComputerPlay = (): Position => {
+        const positions = [
+            Position.topleft,
+            Position.topcenter,
+            Position.topright,
+            Position.centerleft,
+            Position.center,
+            Position.centerright,
+            Position.bottomleft,
+            Position.bottomcenter,
+            Position.bottomright
         ]
 
         while (true) {
@@ -177,8 +131,68 @@ const createComputer = (): Computer => {
         }
 
     }
+    const checkWinner = (gameboard: GameBoard) => {
 
-    return { ...player, choosePosition }
+    }
+    const decideWinner = (winner: Winner) => 'void'
+
+    const changeTurns = () => {
+        if (playerOne.playerTurn) {
+            playerOne.playerTurn = false
+            playerTwo.playerTurn = true
+        }
+        else {
+            playerOne.playerTurn = true
+            playerTwo.playerTurn = false
+        }
+    }
+    const makePlay = (position: Position) => {
+        if (!gameBoard.isPositionMarked(position)) {
+            const currentPlayer = getCurrentPlayer()
+            const mark = currentPlayer.getMark()
+            gameBoard.setMark(position, mark)
+            gameBoard.showBoard()
+            changeTurns()
+        }
+    }
+
+    const reset = () => { }
+
+    return { setGame, makePlay }
+})()
+
+const createPlayer = (playerName: string, playerMark: Mark, type: PlayerType): Player => {
+    const mark: Mark = playerMark
+    const name: string = playerName
+    const playerType: PlayerType = type
+    let playerTurn: boolean = false
+
+    const getMark = () => mark
+    const togglePlayerTurn = () => {
+        if (playerTurn) playerTurn = true
+        else playerTurn = false
+    }
+    return { name, playerType, getMark, togglePlayerTurn, playerTurn }
 }
 
-gameConfig.setPlayerInfo({playerOneName:"Jeu", playerTwoName:"Kel", opponent:"computer"})
+const choosePosition = (gameBoard: GameBoard) => {
+    const positions = [
+        Position.topleft,
+        Position.topcenter,
+        Position.topright,
+        Position.centerleft,
+        Position.center,
+        Position.centerright,
+        Position.bottomleft,
+        Position.bottomcenter,
+        Position.bottomright
+    ]
+
+    while (true) {
+        let randomNum = positions[Math.floor(Math.random() * 10)]
+        if (!gameBoard.isPositionMarked(positions[randomNum])) {
+            return positions[randomNum]
+        }
+    }
+
+}
